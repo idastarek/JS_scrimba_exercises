097a0f441fff9f76c8be990f30c87eb67d4e49a5
Ida <ida.starek@gmail.com>
2024-06-23 19:17:37 +0100

    added second challenge

--- arrays3.js
+++ arrays3.js
@@ -9,3 +9,12 @@ const numbers_doubles = numbers.reduce((acc, num) => {
 
 console.log("doubled numbers", numbers_doubles);
 console.log("numbers", numbers);
+
+
+// multiply each number in the array by 2, using.map();
+
+const numbers2 = [1, 2, 3, 4, 5, 6];
+
+const doubledNumbers2 = numbers.map(num => num * 2);
+console.log('doubled numbers', doubledNumbers2);
+console.log('numbers', numbers2);
